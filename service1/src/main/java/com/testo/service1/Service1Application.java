fbe78abd6585b05b49ef548ecbce7ce5c9646b82
Anton Vyshnevskyi <avyshnevskyi@vmware.com>
2022-02-05 13:46:25 +0200

    init services

--- service1/src/main/java/com/testo/service1/Service1Application.java
+++ service1/src/main/java/com/testo/service1/Service1Application.java
@@ -1,13 +1,13 @@
-package com.testo.oauth2;
+package com.testo.service1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Oauth2Application {
+public class Service1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Oauth2Application.class, args);
+		SpringApplication.run(Service1Application.class, args);
 	}
 
 }
